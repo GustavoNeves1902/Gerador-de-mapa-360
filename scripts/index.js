cf652a11f7fe0c9b1f45ecee54b0de413da0a041
Gustavo Neves <gustavohenrique19022@gmail.com>
2026-01-26 10:24:31 -0300

    Add ScrollReveal animations for images and headers

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('img', {
+    rotate: { x: 100 },
+        duration: 2000,
+        easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('h1, .col-lg-6', {
+    origin: 'left',
+    distance: '50px',
+    duration: '3500'
+})
